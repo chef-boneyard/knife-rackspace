46fed9f65be6e276cc7c65f8fc9aa84402682cd6
Daniel DeLeo <dan@opscode.com>
2011-03-18 12:50:46 -0700

    purge whitespaces

--- lib/chef/knife/rackspace_image_list.rb
+++ lib/chef/knife/rackspace_image_list.rb
@@ -6,9 +6,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,13 +30,13 @@ class Chef
         @highline ||= HighLine.new
       end
 
-      def run 
+      def run
         require 'fog'
 
         connection = Fog::Compute.new(
           :provider => 'Rackspace',
           :rackspace_api_key => Chef::Config[:knife][:rackspace_api_key],
-          :rackspace_username => Chef::Config[:knife][:rackspace_api_username] 
+          :rackspace_username => Chef::Config[:knife][:rackspace_api_username]
         )
 
         image_list = [ h.color('ID', :bold), h.color('Name', :bold) ]
