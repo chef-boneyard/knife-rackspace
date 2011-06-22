471534c0cb739f124c2bd56b8e13eb6a831519c6
Seth Chisamore <schisamo@opscode.com>
2011-06-22 19:52:19 -0400

    [KNIFE_RACKSPACE-3, KNIFE_RACKSPACE-5] support alternate username key name in Chef::Config[:knife]

--- lib/chef/knife/rackspace_image_list.rb
+++ lib/chef/knife/rackspace_image_list.rb
@@ -51,7 +51,7 @@ class Chef
         connection = Fog::Compute.new(
           :provider => 'Rackspace',
           :rackspace_api_key => Chef::Config[:knife][:rackspace_api_key],
-          :rackspace_username => Chef::Config[:knife][:rackspace_api_username],
+          :rackspace_username => Chef::Config[:knife][:rackspace_api_username] || Chef::Config[:knife][:rackspace_username],
           :rackspace_auth_url => Chef::Config[:knife][:rackspace_api_auth_url] || config[:rackspace_api_auth_url]
         )
 
