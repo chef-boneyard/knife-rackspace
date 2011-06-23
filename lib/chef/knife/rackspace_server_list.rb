985e56380fdd37c6ca6dfc31f5e1c41bfc716d6a
Seth Chisamore <schisamo@opscode.com>
2011-06-23 09:44:16 -0400

    make server list output consistent with other knife-* cloud plugins

--- lib/chef/knife/rackspace_server_list.rb
+++ lib/chef/knife/rackspace_server_list.rb
@@ -58,22 +58,22 @@ class Chef
         )
 
         server_list = [
-          ui.color('ID', :bold),
-          ui.color('Name', :bold),
+          ui.color('Instance ID', :bold),
           ui.color('Public IP', :bold),
           ui.color('Private IP', :bold),
           ui.color('Flavor', :bold),
           ui.color('Image', :bold),
+          ui.color('Name', :bold),
           ui.color('State', :bold)
         ]
         connection.servers.all.each do |server|
           server_list << server.id.to_s
-          server_list << server.name
           server_list << (server.public_ip_address == nil ? "" : server.public_ip_address)
           server_list << (server.addresses["private"].first == nil ? "" : server.addresses["private"].first)
-          server_list << (server.flavor == nil ? "" : server.flavor.name.split(/\s/).first)
-          server_list << (server.image == nil ? "" : server.image.name)
-          server_list << (server.state == nil ? "" : server.state)
+          server_list << (server.flavor_id == nil ? "" : server.flavor_id.to_s)
+          server_list << (server.image_id == nil ? "" : server.image_id.to_s)
+          server_list << server.name
+          server_list << (server.state == nil ? "" : server.state.downcase)
         end
         puts ui.list(server_list, :columns_across, 7)
 
