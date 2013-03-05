0695aeb5ac16bc3bb42847ac02391f197eb9e2d2
deniskin82 <dcorol@paypal.com>
2013-03-05 02:05:04 -0500

    Cleanup

--- lib/chef/knife/rackspace_server_reboot.rb
+++ lib/chef/knife/rackspace_server_reboot.rb
@@ -14,8 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Todo:
-# - list more than one server-id
 
 require 'chef/knife/rackspace_base'
 
