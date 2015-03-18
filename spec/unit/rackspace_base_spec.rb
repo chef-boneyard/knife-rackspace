ac1547745cff762deaa59fae1cfe57550bb27f6c
Adam Alboyadjian <adam@vistahigherlearning.com>
2015-03-18 13:53:24 -0400

    Add spec for block_storage_connection.

--- spec/unit/rackspace_base_spec.rb
+++ spec/unit/rackspace_base_spec.rb
@@ -7,32 +7,42 @@ class RackspaceBaseTester < Chef::Knife
   include Chef::Knife::RackspaceBase
 end
 
-describe "auth_endpoint" do
-  it "should select the custom endpoint if specified" do
-    tester = RackspaceBaseTester.new
+describe Chef::Knife::RackspaceBase do
+  let(:tester) { RackspaceBaseTester.new }
 
-    test_url = "http://test-url.com"
-    Chef::Config[:knife][:rackspace_auth_url] = test_url
-    Chef::Config[:knife][:rackspace_region] = :ord
+  describe "auth_endpoint" do
+    it "should select the custom endpoint if specified" do
+      test_url = "http://test-url.com"
+      Chef::Config[:knife][:rackspace_auth_url] = test_url
+      Chef::Config[:knife][:rackspace_region] = :ord
 
-    expect(tester.auth_endpoint).to eq(test_url)
-  end
+      expect(tester.auth_endpoint).to eq(test_url)
+    end
 
-  [:dfw, :ord, :syd].each do |region|
-    it "should pick the US endpoint if the region is #{region}" do
-      tester = RackspaceBaseTester.new
+    [:dfw, :ord, :syd].each do |region|
+      it "should pick the US endpoint if the region is #{region}" do
+        Chef::Config[:knife][:rackspace_auth_url] = nil
+        Chef::Config[:knife][:rackspace_region] = region
+
+        expect(tester.auth_endpoint).to eq(::Fog::Rackspace::US_AUTH_ENDPOINT)
+      end
+    end
+
+    it "should pick the UK end point if the region is :lon" do
       Chef::Config[:knife][:rackspace_auth_url] = nil
-      Chef::Config[:knife][:rackspace_region] = region
+      Chef::Config[:knife][:rackspace_region] = 'lon'
 
-      expect(tester.auth_endpoint).to eq(::Fog::Rackspace::US_AUTH_ENDPOINT)
+      expect(tester.auth_endpoint).to eq(::Fog::Rackspace::UK_AUTH_ENDPOINT)
     end
   end
 
-  it "should pick the UK end point if the region is :lon" do
-    tester = RackspaceBaseTester.new
-    Chef::Config[:knife][:rackspace_auth_url] = nil
-    Chef::Config[:knife][:rackspace_region] = 'lon'
+  describe '#block_storage_connection' do
+    let(:fog_connection) { double(Fog::Rackspace::BlockStorage) }
 
-    expect(tester.auth_endpoint).to eq(::Fog::Rackspace::UK_AUTH_ENDPOINT)
+    it 'returns a Fog block storage connection initialized with block storage connection params' do
+      expect(Fog::Rackspace::BlockStorage).to receive(:new).and_return(fog_connection)
+
+      expect(tester.block_storage_connection).to eq(fog_connection)
+    end
   end
 end
