543968c9eae35b211286564ca93b16a1b3b26d9e
Adam Alboyadjian <adam@vistahigherlearning.com>
2015-03-18 14:00:43 -0400

    Add debugger support for test and dev envs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
 $:.unshift File.expand_path('../../lib', __FILE__)
 require 'chef/knife/bootstrap'
 require 'chef/knife/rackspace_base'
+
+if RUBY_VERSION < '2.0'
+  require 'debugger'
+else
+  require 'byebug'
+end
+
+RSpec.configure do |config|
+  config.color = true
+end
+
